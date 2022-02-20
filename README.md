07ae9f641fa998413d8e8f9bfb2a4034d0e2ee07
Gravity <tevoned@gmail.com>
2022-02-20 18:55:45 -0500

    readme generator bug fix

---
+++ README.md
@@ -0,0 +1,29 @@
+# code-quiz
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+  ## Description:
+  A website that offers, coding boot camp students the opportunity to sharpen their skills by. taking a timed quiz on JavaScript fundamentals that stores high scores Inorder to gauge progress.
+
+  ## Installation:
+  The project was built with javascript, HTML, & CSS
+
+  ## Usage:
+  this project intended use is for developers/aspiring developers to sharpen there skills by answering the given code realted questions 
+
+  ## License:
+  MIT
+
+  ## Contribution:
+  people can contribute to this project by providing additional questions for vistors of the site 
+
+  ## Testing:
+  by performing single user performance test
+
+  ## Additional Info:
+  - Github: [Tai106](https://github.com/Tai106)
+  - Email: taivonedwards1@gmail.com 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,11 +9,19 @@ return "https://img.shields.io/badge/License-"+license+"-blue.svg";
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license=="None") {
+    return " "; }
+return "https://opensource.org/licenses/"+license;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license=="None") {
+    return " "; }
+    return license;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -27,16 +35,22 @@ function generateMarkdown(data) {
   - [Additional Info](#additional-info)
   ## Description:
   ${data.description}
+
   ## Installation:
   ${data.installation}
+
   ## Usage:
   ${data.usage}
+
   ## License:
   ${data.licensing}
+
   ## Contribution:
   ${data.contribution}
+
   ## Testing:
   ${data.testing}
+
   ## Additional Info:
   - Github: [${data.github}](https://github.com/${data.github})
   - Email: ${data.email} `;
